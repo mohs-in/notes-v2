b5bfe7eb183de450828cb5f3fc335f617adb3013
mohs-in <in.mohsin@outlook.com>
2024-05-18 00:55:52 +0530

    update skills data

--- src/data.js
+++ src/data.js
@@ -2,12 +2,12 @@ export default [
     {
         id: 1,
         skill: 'Git',
-        link: 'www',
-        reference: 'example.com',
+        link: 'https://gitnotes.mohs.ink/',
+        reference: 'https://git-scm.com/docs',
     },
     {
         id: 2,
-        skill: 'Tailwind',
+        skill: 'Tailwind CSS',
         link: 'https://mohsinkhan.notion.site/Tailwind-767307b793cd42f1adb1579743210eae',
         reference: 'https://tailwindcss.com/docs/installation',
     }
