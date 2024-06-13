00ef56ce69f99abcde62bca28de979bb81710f19
mohs-in <in.mohsin@outlook.com>
2024-06-13 22:28:53 +0530

    fix height of root element

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import './App.css';
 function App() {
   const skills = data.map(skill => <Skill key={skill.id} {...skill} />)
   return (
-    <div className='flex items-center flex-col bg-gray-900 h-screen'>
+    <div className='flex items-center flex-col bg-gray-900 h-full'>
       <Header />
       <div className='mt-8 flex flex-col md:flex-row'>{skills}</div>
       <Footer />
