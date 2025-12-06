233beb0718abe8c474dc58a581856ce26f45ea36
Mohsin <in.mohsin@outlook.com>
2025-12-06 18:11:25 +0530

    update vite process not found

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    // This line injects a global definition for 'process.env.NODE_ENV'
+    // It reads the environment variable provided by Vite (import.meta.env.MODE)
+    'process.env.NODE_ENV': JSON.stringify(import.meta.env.MODE || 'development'),
+  },
+});
