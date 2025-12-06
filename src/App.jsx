e0fcb8c05e21ab807e9613f50377535056014a0d
Mohsin <in.mohsin@outlook.com>
2025-12-06 18:11:57 +0530

    update notes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import data from '../src/data';
 import Skill from '../components/Skill';
 import Footer from '../components/Footer';
 import './App.css';
+import ExcalidrawViewer from './ExcalidrawViewer';
 
 function App() {
   const skills = data.map(skill => <Skill key={skill.id} {...skill} />)
@@ -10,6 +11,7 @@ function App() {
     <div className='flex items-center flex-col bg-gray-900 h-full sm:h-screen'>
       <Header />
       <div className='mt-8 flex flex-col md:flex-row md:flex-wrap justify-around'>{skills}</div>
+      <ExcalidrawViewer filePath="notes/Streams.excalidraw" />
       <Footer />
     </div>
   )
